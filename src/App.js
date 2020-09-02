b43d3dde287a8e3574cfb4b00b4e72ca0835aec3
Jordan Duran <jordanduran@emv126.derooeinea.com>
2020-09-02 11:07:38 -0400

    Tab highlighting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const Tab = ({ children }) => {
+  const [highlightStyle, setHighlightStyle] = useState({ left: 0, opacity: 0 });
+
+  const handleMoveHighlight = (e) => {
+    // Update highlight style
+    setHighlightStyle({
+      left: e.nativeEvent.layerX - 150,
+    });
+  };
+
+  const handleHideHighlight = (e) => {
+    setHighlightStyle({
+      opacity: 0,
+      left: e.nativeEvent.layerX - 150,
+    });
+  };
+
+  return (
+    <div
+      className="tab"
+      onMouseOut={handleHideHighlight}
+      onMouseMove={handleMoveHighlight}
+    >
+      <div className="hightlight" style={highlightStyle} />
+      {children}
+    </div>
+  );
+};
+
+const App = () => {
   return (
     <div className="app">
       <div className="browser">
         <div className="tabs">
-          <div className="tab">
+          <Tab>
             <a>Home</a>
-          </div>
-          <div className="tab">
+          </Tab>
+          <Tab>
             <a>About</a>
-          </div>
-          <div className="tab">
+          </Tab>
+          <Tab>
             <a>Features</a>
-          </div>
+          </Tab>
         </div>
 
         <div className="viewport">Pages Go Here</div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
