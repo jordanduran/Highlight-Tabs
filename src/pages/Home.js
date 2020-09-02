d0eb2fe8fcad6bc47d67fc72b66832fcc4dcdb26
Jordan Duran <jordanduran@emv126.derooeinea.com>
2020-09-02 11:28:38 -0400

    Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home page!</h1>
+      <p style={{ width: '30rem', textAlign: 'center' }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. In nibh mauris
+        cursus mattis molestie. Venenatis a condimentum vitae sapien
+        pellentesque habitant. Cursus metus aliquam eleifend mi in nulla posuere
+        sollicitudin.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
