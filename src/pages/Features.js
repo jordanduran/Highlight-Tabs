ee246d92e3b005a7a013eacf79ebde65e673f1ca
Jordan Duran <jordanduran@emv126.derooeinea.com>
2020-09-02 11:28:34 -0400

    Features page

---
+++ src/pages/Features.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Features = () => {
+  return (
+    <div>
+      <h1>Welcome to the Features page!</h1>
+      <p style={{ width: '25rem', textAlign: 'center', marginLeft: '4rem' }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. In nibh mauris
+        cursus mattis molestie. Venenatis a condimentum vitae sapien
+        pellentesque habitant. Cursus metus aliquam eleifend mi in nulla posuere
+        sollicitudin.
+      </p>
+    </div>
+  );
+};
+
+export default Features;
